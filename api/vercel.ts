10db3620f14446e1f37c929a9a27741654a03a6b
andersonmerten <anderson.merten@gmail.com>
2025-05-19 16:27:54 -0300

    add some vercel configs

---
+++ api/vercel.ts
@@ -0,0 +1,41 @@
+import { NestFactory } from '@nestjs/core';
+import type { Handler, HandlerContext, HandlerEvent } from '@netlify/functions';
+import { AppModule } from '../src/app.module';
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+  return app;
+};
+
+// Cache da instância do NestJS
+let cachedApp: any;
+
+export const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext,
+) => {
+  if (!cachedApp) {
+    cachedApp = await bootstrap();
+  }
+
+  const expressApp = cachedApp.getHttpAdapter().getInstance();
+
+  return new Promise((resolve) => {
+    expressApp(event, context, (err: any, result: any) => {
+      if (err) {
+        resolve({
+          statusCode: 500,
+          body: JSON.stringify({ error: err.message }),
+        });
+        return;
+      }
+      resolve(
+        result || {
+          statusCode: 200,
+          body: JSON.stringify({ message: 'Success' }),
+        },
+      );
+    });
+  });
+};
